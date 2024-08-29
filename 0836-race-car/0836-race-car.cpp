0561fad4d9ef109c9b9340483b9e71364553070f
Adit Parekh <96000047+AditParekh22@users.noreply.github.com>
2024-08-29 22:52:13 +0530

    Time: 0 ms (100%), Space: 8 MB (88.28%) - LeetHub

---
+++ 0836-race-car/0836-race-car.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int solve(int target, vector<int> &dp){
+        if(target == 0)
+            return 0;
+        if(dp[target] != -1)
+            return dp[target];
+
+        int m = log2(target) + 1;
+        if(target == (1<<m)-1)
+            return m;
+
+        // Case B1
+        int ans = m + 1 + solve((1<<m) - 1 - target, dp);
+
+        for(int i=0; i<= m - 2; i++){
+            ans = min(ans, m + i + 1 + solve(target-(1<<(m-1)) + (1<<i), dp));
+        }
+        return dp[target] = ans;
+    }
+    int racecar(int target) {
+        vector<int> dp(target+1, -1);
+        return solve(target, dp);
+    }
+};
