f5e752dc4095c2d0a0994e0d10db6d1db3edc5c9
Adit Parekh <96000047+AditParekh22@users.noreply.github.com>
2024-09-06 17:35:17 +0530

    Time: 80 ms (19.05%), Space: 23.4 MB (30.71%) - LeetHub

--- 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.cpp
+++ 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.cpp
@@ -1,40 +1,44 @@
 class Solution {
 public:
     vector<int> findSubstring(string s, vector<string>& words) {
-        unordered_map<string, int> dict;
-        for(auto &w: words) dict[w]++;
-        vector<int> res;
-        int n = s.length(), m = words[0].length(), w = words.size();
-        
-        for(int k=0; k<m; k++) {                     // O(m)
-            unordered_map<string, int> seen;
+        unordered_map<string, int>  cnt;
+        for(auto w : words)
+            cnt[w]++;
+        int n = s.size(), m = words[0].size();
+        vector<int> ans;
+        for(int k=0; k<m; k++){
+            cout << "k : " << k << endl; 
+            unordered_map<string, int>  seen;
             int left = k, currLen = 0;
-            for(int i=left; i+m<=n; i+=m) {          // 2*O(n/m)
-                string temp = s.substr(i, m);        // O(m)
-                if(!(dict.count(temp))) {
+            for(int i=left; i<n; i+=m){
+                string curr = s.substr(i, m);
+                cout << i << " " << curr << endl;
+                if(cnt.count(curr)){
+                    seen[curr]++;
+                    currLen++;
+                    while(seen[curr] > cnt[curr]){
+                        string frt = s.substr(left, m);
+                        cout << "\t" << frt << endl;
+                        seen[frt]--;
+                        currLen--;
+                        left+=m;
+                    }
+                }
+                else{
                     seen.clear();
                     currLen = 0;
                     left = i+m;
-                } else {
-                    seen[temp]++;
-                    currLen++;
-                    if(seen[temp] > dict[temp]) {
-                        while(seen[temp] > dict[temp]) {
-                            string temp1 = s.substr(left, m);
-                            seen[temp1]--;
-                            currLen--;
-                            left += m;
-                        }
-                    }
                 }
-                if(currLen == w) {
-                    res.push_back(left);
+
+                if(currLen == words.size()){
+                    ans.push_back(left);
                     seen[s.substr(left, m)]--;
                     currLen--;
                     left += m;
                 }
             }
+            
         }
-        return res;
+        return ans;
     }
 };
