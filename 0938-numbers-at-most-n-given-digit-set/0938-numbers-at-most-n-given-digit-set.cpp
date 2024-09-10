4b18925c3af3a77623095baddc32c02830da3948
Adit Parekh <96000047+AditParekh22@users.noreply.github.com>
2024-09-10 19:16:33 +0530

    Time: 0 ms (100%), Space: 10.5 MB (17.27%) - LeetHub

--- 0938-numbers-at-most-n-given-digit-set/0938-numbers-at-most-n-given-digit-set.cpp
+++ 0938-numbers-at-most-n-given-digit-set/0938-numbers-at-most-n-given-digit-set.cpp
@@ -1,31 +1,42 @@
 class Solution {
 public:
-    int mod=1e9+7;
-    int dp[32][2];
-    int fun(int i,int tight,string& s,string &digit)
-    {
-        if(i==s.size())
+    int solve(vector<int> &d, string s, int ind, int flag, vector<vector<int> > &dp){
+        if(ind >= s.size())
             return 1;
-        if(dp[i][tight]!=-1)
-            return dp[i][tight];
-        int ans=0;
-        char mx='9';
-        if(tight)
-            mx=min(mx,s[i]);
-        for(int j=0;j<digit.size() and digit[j]<=mx;j++)
-            ans+=fun(i+1,(tight and digit[j]==mx)?1:0,s,digit);
-        return dp[i][tight]=ans;
+
+        if(dp[ind][flag] != -1)
+            return dp[ind][flag];
+
+        int limit = (flag == 1 ? s[ind]-'0' : d[d.size()-1]);
+        int ans = 0;
+        for(int i=0; i<d.size(); i++){
+            if(d[i]>limit)
+                break;
+            ans += solve(d, s, ind+1, flag & (d[i] == limit), dp); 
+        }
+
+        return dp[ind][flag] = ans;
     }
     int atMostNGivenDigitSet(vector<string>& digits, int n) {
-        int ans=0;
-        string s=to_string(n);
-        memset(dp,-1,sizeof(dp));
-        string digit;
-        for(auto it:digits)
-            digit.push_back(it[0]);
-        ans=fun(0,1,s,digit);
-        for(int i=1;i<s.size();i++)
-            ans+=fun(i,0,s,digit);
+        vector<int> d;
+        for(auto it : digits)
+            d.push_back(stoi(it));
+
+        string s = to_string(n);
+        int len = s.size();
+
+        vector<vector<int> > dp(len, vector<int>(2, -1));
+
+        int ans = solve(d, s, 0, 1, dp);
+
+        int pro = d.size();
+        for(int i = 1; i<len; i++){
+            ans += pro;
+            pro = pro*d.size();
+        }
+
         return ans;
+
+        
     }
 };
