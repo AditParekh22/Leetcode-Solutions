7bc29dada902250433d469043926e7dc116c1ee8
Adit Parekh <96000047+AditParekh22@users.noreply.github.com>
2024-09-13 18:14:50 +0530

    Time: 108 ms (59.17%), Space: 96.8 MB (66.41%) - LeetHub

--- 3152-maximum-value-of-an-ordered-triplet-ii/3152-maximum-value-of-an-ordered-triplet-ii.cpp
+++ 3152-maximum-value-of-an-ordered-triplet-ii/3152-maximum-value-of-an-ordered-triplet-ii.cpp
@@ -6,25 +6,17 @@ public:
         vector<int> pref(n), suff(n);
         
         pref[0] = 0, suff[n-1] = 0;
-        for(int i=1; i<n; i++){
+        for(int i=1; i<n; i++)
             pref[i] = max(nums[i-1], pref[i-1]);
-        }
-        for(int i=n-2; i>=0; i--){
+
+        for(int i=n-2; i>=0; i--)
             suff[i] = max(nums[i+1], suff[i+1]);
-        }
-
-        for(auto it : pref)
-            cout << it << " ";
-        cout << endl;
-
-        for(auto it : suff)
-            cout << it << " ";
-        cout << endl;
+        
 
         long long ans = INT_MIN;
-        for(int i=0; i<n; i++){
+        for(int i=0; i<n; i++)
             ans = max(ans, ((ll)(pref[i] - nums[i]) * (ll)suff[i]));
-        }
+        
 
         return ans;
     }
