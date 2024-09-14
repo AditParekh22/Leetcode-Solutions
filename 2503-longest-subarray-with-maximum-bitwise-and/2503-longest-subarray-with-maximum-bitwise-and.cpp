f68d97eff165156cc3f6a28d91c47cab56e33dd7
Adit Parekh <96000047+AditParekh22@users.noreply.github.com>
2024-09-14 12:16:22 +0530

    Time: 117 ms (12.94%), Space: 84.9 MB (34.21%) - LeetHub

--- 2503-longest-subarray-with-maximum-bitwise-and/2503-longest-subarray-with-maximum-bitwise-and.cpp
+++ 2503-longest-subarray-with-maximum-bitwise-and/2503-longest-subarray-with-maximum-bitwise-and.cpp
@@ -2,31 +2,18 @@ class Solution {
 public:
     int longestSubarray(vector<int>& nums) {
         int n = nums.size();
-        unordered_map<int, int> prev;
-        int maxi = INT_MIN, ans = 0;
+        cout << n << endl;
+        int maxi = *max_element(nums.begin(), nums.end());
+        int ans= 0, length = 0;
 
         for(int i=0; i<n; i++){
-            // cout << "i : " << i << endl;
-            unordered_map<int, int> curr;
-            for(auto it : prev)
-                curr[it.first & nums[i]] = it.second+1;
-            
-            if(curr.find(nums[i]) == curr.end())
-                curr[nums[i]] = 1;
-
-            for(auto it : curr){
-                // cout << it.first << " " << it.second << endl;
-                if(it.first > maxi){
-                    // cout << it.first << " x " << it.second << endl;
-                    maxi = it.first;
-                    ans = it.second;
-                }
-                else if(it.first == maxi)
-                    ans = max(ans, it.second);
-            }
-
-            prev = curr;
-        }   
+            if(nums[i] == maxi)
+                ans = max(ans, ++length);
+            else
+                length = 0;
+        }
         return ans;
+
+
     }
 };
